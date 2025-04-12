4957dcc4ab2f2a1abc669bf64c2a26105782c218
Bryan Linares <blinar55@gmail.com>
2025-04-11 20:10:41 -0700

    asset moving

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function RotatingMesh() {
   const meshRef = useRef()
-  const gltf = useLoader(GLTFLoader, "src/assets/Poimandres.gltf")
+  //const gltf = useLoader(GLTFLoader, "/assets/Poimandres.gltf")
   // Rotate the mesh on every frame
   useFrame(() => {
     if (meshRef.current) {
